428bd3d2bc86cc9e2fe30cc534f55d91590b8084
likaiqiang <likaiqiang.frontend@gmail.com>
2023-03-16 08:17:33 +0800

    优化删除会话逻辑

--- client/src/views/list/item.jsx
+++ client/src/views/list/item.jsx
@@ -3,6 +3,7 @@ import {AntOutline, RightOutline} from "antd-mobile-icons";
 import {useLongPress} from 'ahooks';
 import {useContext, useRef} from "react";
 import {useNavigate} from "react-router-dom";
+import cloneDeep from 'lodash.clonedeep'
 import Context from '../../context'
 
 const Item = (props) => {
@@ -14,8 +15,9 @@ const Item = (props) => {
         Modal.confirm({
             content: '删除确认',
             onConfirm: () => {
-                delete cache[data.convId]
-                setCache(cache)
+                const copyCache = cloneDeep(cache)
+                delete copyCache[data.convId]
+                setCache(copyCache)
                 Toast.show({
                     icon: 'success',
                     content: '删除成功',
